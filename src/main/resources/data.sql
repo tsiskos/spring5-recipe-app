1dbda77a70525bd88612fc58ec0edaa6f79480af
Tsiskomichelis Stelios <tsiskost@gmail.com>
2020-01-20 21:24:13 +0200

    Data initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO category (description) VALUES ('Greek');
+INSERT INTO category (description) VALUES ('Mexican');
+INSERT INTO category (description) VALUES ('Chinese');
+INSERT INTO category (description) VALUES ('Gourmet');
+INSERT INTO category (description) VALUES ('Snack');
+
+INSERT INTO unit_of_measure (uom) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (uom) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (uom) VALUES ('Cup');
+INSERT INTO unit_of_measure (uom) VALUES ('Pinch');
