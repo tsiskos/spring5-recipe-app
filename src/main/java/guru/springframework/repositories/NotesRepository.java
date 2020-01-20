299e08a7380a49e66763a0f504935bbe631f8421
Tsiskomichelis Stelios <tsiskost@gmail.com>
2020-01-20 20:42:05 +0200

    Add repositories

---
+++ src/main/java/guru/springframework/repositories/NotesRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.repositories;
+
+import guru.springframework.domain.Notes;
+import org.springframework.data.repository.CrudRepository;
+
+public interface NotesRepository extends CrudRepository<Notes,Long> {
+}
